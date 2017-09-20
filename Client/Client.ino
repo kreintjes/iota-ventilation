f241a041e3fd0e9fc0fa4612652b619e1dc4f4d2
Kevin Reintjes <kreintjes@gmail.com>
2017-09-20 23:27:43 +0200

    Add feature to set different on/off humidities to Sensor
    This makes it possible to set a gap between turning the ventilation on and off.
    For example we start the ventilation at an humidity of 60, but we keep it running
    until it drops below 50. This prevents the humidity from hanging around the turn
    point, causing the ventilation to be switched on/off many times after each other.

--- Client/Client.ino
+++ Client/Client.ino
@@ -3,8 +3,11 @@
 #define WIFI_SSID           "wicked"
 #define WIFI_PASSWORD       "thereisnospoon"
 
-#define VENTILATION_LOW     50
-#define VENTILATION_HIGH    55
+#define VENTILATION_LOW_ON      55 // Low mode will be turned on when humidity is at least this value.
+#define VENTILATION_LOW_OFF     50 // Low mode will only be turned of when humidity is below this value. Use same value as on if you do not want an on/off gap.
+
+#define VENTILATION_HIGH_ON     60 // High mode will be turned on when humidity is at least this value.
+#define VENTILATION_HIGH_OFF    55 // High mode will only be turned of when humidity is below this value. Use same value as on if you do not want an on/off gap.
 
 #define DELAY 100
 #define DHT_READ_INTERVAL 6000
@@ -39,10 +42,10 @@ void loop() {
 
 void loopLogic() {
   String newState;
-  if(humidity >= VENTILATION_HIGH) {
+  if(humidity >= VENTILATION_HIGH_ON || (lastState == "high" && humidity >= VENTILATION_HIGH_OFF)) {
     newState = "high";
     turnLedOn();
-  } else if (humidity >= VENTILATION_LOW && humidity < VENTILATION_HIGH) {
+  } else if (humidity >= VENTILATION_LOW_ON || ((lastState == "high" || lastState == "low") && humidity >= VENTILATION_LOW_OFF)) {
     newState = "low";
     blinkLed(500);
   } else {
